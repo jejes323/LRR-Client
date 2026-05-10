5a0615d3894f96e4dfab0e818990140557ddaf78
leavingisfine <jejes323@gmail.com>
2026-05-10 16:08:33 +0900

    feat(login): YAML 기반 로그인 인증

---
+++ LRR/src/main/java/deu/cse/lrr/LRR.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package deu.cse.lrr;
+
+import deu.cse.lrr.login.Login;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author seo
+ */
+public class LRR {
+
+    private static final Logger logger = Logger.getLogger(LRR.class.getName());
+
+    public static void main(String[] args) {
+        // Nimbus Look and Feel 설정
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ReflectiveOperationException | javax.swing.UnsupportedLookAndFeelException ex) {
+            logger.log(Level.SEVERE, null, ex);
+        }
+
+        // Login JFrame 실행
+        java.awt.EventQueue.invokeLater(() -> new Login().setVisible(true));
+    }
+}
